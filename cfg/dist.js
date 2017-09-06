1c484b122a66cba383f7c7c0578a9f3875c6fb18
ROG\zhyang <zhenghe_yang@hotmail.com>
2017-09-07 00:34:53 +0800

    创建图片

--- cfg/dist.js
+++ cfg/dist.js
@@ -37,10 +37,6 @@ config.module.loaders.push({
     config.additionalPaths,
     [ path.join(__dirname, '/../src') ]
   )
-},
-{
-  test: /\.json$/,
-  loader: 'json-loader'
 });
 
 module.exports = config;
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,10 @@ require('styles/App.css');
 import React from 'react';
 
 //获取图片相关的数据
-let imageDatas = require('../data/imageDatas.json');
+let imageDatas = require('json!../data/imageDatas.json');
 //利用自执行函数，将图片名信息转成图片URL路径信息
 imageDatas=(function genImageURL(imageDatasArr) {
-  for(let i=0;i<imageDatasArr.length;i++){
+  for(var i=0;i<imageDatasArr.length;i++){
     let singleImageData=imageDatasArr[i];
     singleImageData.imageURL=require('../images/'+singleImageData.fileName);
      imageDatasArr[i]=singleImageData;
@@ -15,14 +15,40 @@ imageDatas=(function genImageURL(imageDatasArr) {
   return imageDatasArr;
 })(imageDatas);
 
+class ImgFigure extends React.Component {
+  render() {
+    return (
+      <figure>
+          <img src={this.props.data.imageURL}
+                alt={this.props.data.title}/>
+          <figcaption>
+              <h2>{this.props.data.title}</h2>
+          </figcaption>
+      </figure>
+    );
+  }
+}
 
 class GalleryByReactApp extends React.Component {
+
+
   render() {
+
+      let controllerUnits=[];
+      let imgFigures=[];
+
+      imageDatas.forEach((value, index) =>{
+
+      imgFigures.push(<ImgFigure data = {value} key={'imgFigures'+index}/>);
+
+    });
     return (
       <section className="stage">
         <section className="img-sec">
+          {imgFigures}
         </section>
         <nav className="controller-nav">
+          {controllerUnits}
         </nav>
       </section>
     );
